29011396617140dfaa4e16dde871685cab2d994d
Dzmitry Martavoi <Dzmitry Martavoi>
2016-06-27 02:33:11 +0300

    base infrastructure & page design layout

---
+++ src/home.core/gulpfile.js
@@ -0,0 +1,20 @@
+﻿var gulp = require("gulp"),
+    less = require("gulp-less"),
+    del = require("del");
+
+gulp.task("clean", function () {
+    return del("wwwroot/**/*");
+});
+
+gulp.task("copy:modules", function () {
+    return gulp.src("./node_modules/**/*")
+		.pipe(gulp.dest("./wwwroot/modules"));
+});
+
+gulp.task("less", function () {
+    return gulp.src("./Views/Home/home.less")
+      .pipe(less())
+      .pipe(gulp.dest("./wwwroot/Home"));
+});
+
+gulp.task("default", ["copy:modules", "less"]);
